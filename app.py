984f094b6872b978852dfcccbf4a5cd601e42266
Gabriel Marcelino <dgabrielmarcelino@gmail.com>
2025-12-05 11:19:31 -0300

    correçao para o deploy

--- app.py
+++ app.py
@@ -2,19 +2,29 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_mantine_components as dmc 
 
+# 1. ATIVA O REACT 18 (Obrigatório para DMC novo funcionar)
+dash._dash_renderer._set_react_version("18.2.0")
+
+# 2. CARREGA O CSS DO MANTINE (Obrigatório, senão fica invisível)
+stylesheets = [
+    dbc.themes.CERULEAN,
+    "https://unpkg.com/@mantine/core@7.10.0/styles.css",
+    "https://unpkg.com/@mantine/dates@7.10.0/styles.css"
+]
+
 app = dash.Dash(__name__, 
-                external_stylesheets=[dbc.themes.CERULEAN,], 
+                external_stylesheets=stylesheets, 
                 suppress_callback_exceptions=True)
 
 server = app.server
 
-# --- O LAYOUT DEVE ESTAR AQUI (FORA DO IF) ---
+# 3. SEU LAYOUT
 app.layout = dmc.MantineProvider(
     children=[
-        # ... todo o resto do seu código ...
+        # --- COLE O SEU CONTEÚDO AQUI DENTRO ---
+        # (Seus gráficos, dmc.Container, etc)
     ]
 )
-# ---------------------------------------------
 
 if __name__ == '__main__':
     app.run_server(debug=True)
